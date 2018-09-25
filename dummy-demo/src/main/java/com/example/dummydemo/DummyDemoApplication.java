fa6442b1372d568a458c7bac31bf88fe9728215b
Livent <Livented@users.noreply.github.com>
2018-09-25 18:48:15 +0700

    Initial commit

---
+++ dummy-demo/src/main/java/com/example/dummydemo/DummyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.dummydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyDemoApplication.class, args);
+	}
+}
